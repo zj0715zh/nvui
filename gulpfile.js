ab7f2fdb72bea215bee1cffbc3a71c56676ed98d
itKingOne <1372780005@qq.com>
2018-04-10 11:14:41 +0800

    Signed-off-by: itKingOne <1372780005@qq.com>

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,21 @@ var cssmin = require('gulp-cssmin');
 var salad = require('postcss-salad')(require('./salad.config.json'));
 
 gulp.task('compile', function() {
-  return gulp.src('./packages/src/*.css')
+  return gulp.src('./packages/theme-default/src/*.css')
     .pipe(postcss([salad]))
     .pipe(cssmin())
-    .pipe(gulp.dest('./packages/lib'));
+    .pipe(gulp.dest('./packages/theme-default/lib'));
 });
 
 gulp.task('copyfont', function() {
-  return gulp.src('./packages/src/fonts/**')
+  return gulp.src('./packages/theme-default/src/fonts/**')
     .pipe(cssmin())
-    .pipe(gulp.dest('./packages/lib/fonts'));
+    .pipe(gulp.dest('./packages/theme-default/lib/fonts'));
 });
 
 
 gulp.task('build', ['compile', 'copyfont']);
 gulp.task('watch', function () {
-  gulp.watch('./packages/src/*.css', ['compile']);
+  gulp.watch('./packages/theme-default/src/*.css', ['compile']);
 });
 gulp.task('default', ['compile', 'copyfont']);
