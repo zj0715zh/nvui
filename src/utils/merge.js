7e89a2c8da61828c0a579c260466574ca31d8548
itKingOne <1372780005@qq.com>
2018-11-13 11:09:53 +0800

    Signed-off-by: itKingOne <1372780005@qq.com>

--- src/utils/merge.js
+++ src/utils/merge.js
@@ -1,9 +1,9 @@
 export default function(target) {
-  for (let i = 1, j = arguments.length; i < j; i++) {
-    let source = arguments[i] || {};
-    for (let prop in source) {
+  for (var i = 1, j = arguments.length; i < j; i++) {
+    var source = arguments[i] || {};
+    for (var prop in source) {
       if (source.hasOwnProperty(prop)) {
-        let value = source[prop];
+        var value = source[prop];
         if (value !== undefined) {
           target[prop] = value;
         }
